862de61ac50d2a3bcc4f29ffa7baaa5fc4d1c13b
jinhao <jinhao_@hotmail.com>
2015-12-14 22:25:16 +0800

    typos again -_-

--- nineku/settings/base.py
+++ nineku/settings/base.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 import dj_database_url
+from django.conf import settings
+
+DATABASES = settings.DATABASES
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_URL = '/static/'
