4e7944ff2d1523ecf7b564f9d263e289bb6462cc
jinhao <jinhao_@hotmail.com>
2015-12-18 00:20:17 +0800

    changed allowed host

--- nineku/settings/base.py
+++ nineku/settings/base.py
@@ -25,7 +25,7 @@ STATIC_URL = '/static/'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [".herokuapp.com"]
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
