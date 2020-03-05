d17b9b840b57a6e8cd4a3283c8b79da5a6253b33
Chan Jin Hao <chan_jinhao@ensigninfosecurity.com>
2020-03-05 15:36:25 +0800

    fixed render

--- nineku/views.py
+++ nineku/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponseRedirect, HttpResponse
-from django.shortcuts import render, render_to_response, get_object_or_404
+from django.shortcuts import render, get_object_or_404
 from .models import *
 from .forms import *
 from .auth import *
@@ -172,7 +172,7 @@ def register_user(request):
     else:
         args['form'] = RegistrationForm()
 
-    return render_to_response('registration/register.html', args, context_instance=RequestContext(request))
+    return render(request, 'registration/register.html', args)
 
 def register_confirm(request, activation_key):
     args = {}
@@ -188,7 +188,7 @@ def register_confirm(request, activation_key):
 
     #check if the activation key has expired, if it hase then render confirm_expired.html
     if user_profile.key_expires < timezone.now():
-        return render_to_response('registration/confirm_expired.html', args, context_instance=RequestContext(request))
+        return render(request, 'registration/confirm_expired.html', args)
     #if the key hasn't expired save user and set him as active and render some template to confirm activation
     user = user_profile.user
 
@@ -198,7 +198,7 @@ def register_confirm(request, activation_key):
         user.is_active = True
 
     user.save()
-    return render_to_response('registration/confirm.html', args, context_instance=RequestContext(request))
+    return render(request, 'registration/confirm.html', args)
 
 def viewUserPosts(request):
     username = request.session['username']
